ab7d4693b8890a2c061ad3903beb9125d8585b2d
jakecumber1 <jakecumber1@gmail.com>
2025-08-21 15:34:00 -0500

    Implemented Schlick's approximation to determine how reflective a dielectric is. Main generates a bubble of air within a sphere of glass to show off this effect on the left of the image.

--- Ray Tracer/main.cpp
+++ Ray Tracer/main.cpp
@@ -16,14 +16,17 @@ int main() {
 	//define material types
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
 	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_left = make_shared<dielectric>(1.50);
+	//air inside our material left (glass)
+	auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 	//ground is just a giant sphere pushed down and given ground material
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
 	//sphere showing off lambertian material
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
-	//sphere showing off metal material 1 (left)
+	//sphere showing off dielectics
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
 	//sphere showing off metal material 2 (right)
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
--- Ray Tracer/material.h
+++ Ray Tracer/material.h
@@ -53,4 +53,59 @@ private:
 	double fuzz;
 };
 
+/*Some helpful definitions for defining materials
+mainly for understanding dielectrics when we implement that material:
+-reflected ray = ray hits a surface and bounces off in a new direction
+-refracted ray = ray bends when it transforms from a material's surroundings into the material itself
+refracted rays are why pencils look bent when placed partially in water as an example
+-refraction index = the amount a refracted ray bends (glass is 1.5-1.7, diamond 2.4, air 1.000293)
+For transparent materials within transparent materials, the refraction can be described with a relative refraction index
+-relative refraction index = refractive index of the embedded material / refractive index of surrounding material
+for example a glass ball under water would have a relative reflective index of 1.125 (1.5/1.333)
+-total internal reflection: when traveling from a denser medium to a less dense one at the right angle, all the rays are reflected bac into the denser medium
+one example to book gives is water to air, if you're looking straight up, you can see through fine, but if you are near the surface looking sideways,
+the boundary acts like a mirror*/
+
+
+
+class dielectric : public material {
+public:
+	dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+	bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+		//for glass it absorbs nothing, so attenuation is always 1
+		attenuation = color(1.0, 1.0, 1.0);
+		double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+		double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+		bool cannot_refract = ri * sin_theta > 1.0;
+		vec3 direction;
+		//we are passed the critical angle, so we should reflect instead of refract
+		if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
+			direction = reflect(unit_direction, rec.normal);
+		}
+		else {
+			direction = refract(unit_direction, rec.normal, ri);
+		}
+
+		scattered = ray(rec.p, direction);
+		return true;
+	}
+private:
+	//refracting index in vacuum or air, or ratio of materials refractive index
+	//over refactive index of enclosing material
+	double refraction_index;
+	//Uses Schlick's approximation to determine reflectivity based on angle
+	static double reflectance(double cosine, double refraction_index) {
+		//schlick's approximation is a really cheap way to deterimine reflectance.
+		//read more about the formula here: https://en.wikipedia.org/wiki/Schlick%27s_approximation
+		auto r0 = (1 - refraction_index) / (1 + refraction_index);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+	}
+};
+
 #endif // !MATERIAL_H
